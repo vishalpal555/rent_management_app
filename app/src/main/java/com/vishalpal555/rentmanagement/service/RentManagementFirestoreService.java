b27da6b1914f82cee21ec29d08a0e2bb5612420f
Vishal Pal <vishalpal555@gmail.com>
2023-09-14 16:30:23 +0530

    added employees and rooms append and remove in db service

--- app/src/main/java/com/vishalpal555/rentmanagement/service/RentManagementFirestoreService.java
+++ app/src/main/java/com/vishalpal555/rentmanagement/service/RentManagementFirestoreService.java
@@ -20,6 +20,9 @@ public class RentManagementFirestoreService {
     DocumentReference firestoreDocument;
     private static final String RENT_MANAGEMENT_FIREBASE_REF = "rent_management";
     private static final String RENTMANAGEMENT_ADMINS_FIELD_STRING = "admins";
+    private static final String RENTMANAGEMENT_EMPLOYEES_FIELD_STRING = "employees";
+    private static final String RENTMANAGEMENT_ROOMS_FIELD_STRING = "roomsList";
+    private static final String RENTMANAGEMENT_TENANTS_FIELD_STRING = "tenantsList";
 
     public RentManagementFirestoreService(){
         firestore = FirebaseFirestore.getInstance();
@@ -47,6 +50,7 @@ public class RentManagementFirestoreService {
         });
     }
 
+    //admins service
     public void appendAdmins(Activity activity, String rentManagementId, Set<String> newAdminSet){
         firestoreDocument = firestoreCollection.document(rentManagementId);
         firestoreDocument.get().addOnCompleteListener(getTask -> {
@@ -76,7 +80,7 @@ public class RentManagementFirestoreService {
 
     public void removeAdmins(Activity activity, String rentManagementId, Set<String> removeAdminSet){
         firestoreDocument = firestoreCollection.document(rentManagementId);
-        removeAdminSet.remove(rentManagementId);    //removing default admin id
+        removeAdminSet.remove(rentManagementId);    //removing default admin id from removeAdminSet
         firestoreDocument.get().addOnCompleteListener(getTask -> {
             if(getTask.getResult().exists()){
                 List<String> prevAdmins = (ArrayList<String>) getTask.getResult().getData().get(RENTMANAGEMENT_ADMINS_FIELD_STRING);
@@ -99,6 +103,116 @@ public class RentManagementFirestoreService {
             }
         }).addOnFailureListener(e -> {
             Log.e(TAG, "removeAdmins: firestoreDocument.get() ", e);
+        });
+    }
+
+    //employees service
+    public void appendEmployees(Activity activity, String rentManagementId, Set<String> newEmployeeSet){
+        firestoreDocument = firestoreCollection.document(rentManagementId);
+        firestoreDocument.get().addOnCompleteListener(getTask -> {
+            if(getTask.getResult().exists()) {
+                List<String> prevEmployees = (ArrayList<String>) getTask.getResult().getData().get(RENTMANAGEMENT_EMPLOYEES_FIELD_STRING);
+                if(newEmployeeSet.addAll(prevEmployees)) {
+                    firestoreDocument.update(RENTMANAGEMENT_EMPLOYEES_FIELD_STRING, new ArrayList<>(newEmployeeSet))
+                            .addOnCompleteListener(updateTask -> {
+                                if (updateTask.isSuccessful()) {
+                                    Log.i(TAG, "appendEmployees: firestoreDocument.update() added employees" + newEmployeeSet);
+                                    Toast.makeText(activity, "Employees added", Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .addOnFailureListener(e -> {
+                                Log.e(TAG, "appendEmployees: firestoreDocument.update() ", e);
+                            });
+                } else {
+                    Toast.makeText(activity, "Employees already present", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Log.e(TAG, "appendEmployees: document doesn't exists ");
+            }
+        }).addOnFailureListener(e -> {
+            Log.e(TAG, "appendEmployees: firestoreDocument.get() ", e);
+        });
+    }
+
+    public void removeEmployees(Activity activity, String rentManagementId, Set<String> removeEmployeeSet){
+        firestoreDocument = firestoreCollection.document(rentManagementId);
+        firestoreDocument.get().addOnCompleteListener(getTask -> {
+            if(getTask.getResult().exists()){
+                List<String> prevEmployees = (ArrayList<String>) getTask.getResult().getData().get(RENTMANAGEMENT_EMPLOYEES_FIELD_STRING);
+                if(prevEmployees.removeAll(removeEmployeeSet)) {
+                    firestoreDocument.update(RENTMANAGEMENT_EMPLOYEES_FIELD_STRING, prevEmployees)
+                            .addOnCompleteListener(updateTask -> {
+                                if (updateTask.isSuccessful()) {
+                                    Log.i(TAG, "removeEmployees: firestoreDocument.update() removed employees " + removeEmployeeSet);
+                                    Toast.makeText(activity, "Employees removed", Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .addOnFailureListener(e -> {
+                                Log.e(TAG, "removeEmployees: firestoreDocument.update() ", e);
+                            });
+                } else {
+                    Toast.makeText(activity, "Employees not present", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Log.e(TAG, "removeEmployees: document doesn't exists ");
+            }
+        }).addOnFailureListener(e -> {
+            Log.e(TAG, "removeEmployees: firestoreDocument.get() ", e);
+        });
+    }
+
+    //rooms list service
+    public void appendRooms(Activity activity, String rentManagementId, Set<String> newRoomSet){
+        firestoreDocument = firestoreCollection.document(rentManagementId);
+        firestoreDocument.get().addOnCompleteListener(getTask -> {
+            if(getTask.getResult().exists()) {
+                List<String> prevRooms = (ArrayList<String>) getTask.getResult().getData().get(RENTMANAGEMENT_ROOMS_FIELD_STRING);
+                if(newRoomSet.addAll(prevRooms)) {
+                    firestoreDocument.update(RENTMANAGEMENT_ROOMS_FIELD_STRING, new ArrayList<>(newRoomSet))
+                            .addOnCompleteListener(updateTask -> {
+                                if (updateTask.isSuccessful()) {
+                                    Log.i(TAG, "appendRooms: firestoreDocument.update() added rooms" + newRoomSet);
+                                    Toast.makeText(activity, "Rooms added", Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .addOnFailureListener(e -> {
+                                Log.e(TAG, "appendRooms: firestoreDocument.update() ", e);
+                            });
+                } else {
+                    Toast.makeText(activity, "Rooms already present", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Log.e(TAG, "appendRooms: document doesn't exists ");
+            }
+        }).addOnFailureListener(e -> {
+            Log.e(TAG, "appendRooms: firestoreDocument.get() ", e);
+        });
+    }
+
+    public void removeRooms(Activity activity, String rentManagementId, Set<String> removeRoomSet){
+        firestoreDocument = firestoreCollection.document(rentManagementId);
+        firestoreDocument.get().addOnCompleteListener(getTask -> {
+            if(getTask.getResult().exists()){
+                List<String> prevRooms = (ArrayList<String>) getTask.getResult().getData().get(RENTMANAGEMENT_ROOMS_FIELD_STRING);
+                if(prevRooms.removeAll(removeRoomSet)) {
+                    firestoreDocument.update(RENTMANAGEMENT_ROOMS_FIELD_STRING, prevRooms)
+                            .addOnCompleteListener(updateTask -> {
+                                if (updateTask.isSuccessful()) {
+                                    Log.i(TAG, "removeRooms: firestoreDocument.update() removed rooms " + removeRoomSet);
+                                    Toast.makeText(activity, "Rooms removed", Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .addOnFailureListener(e -> {
+                                Log.e(TAG, "removeRooms: firestoreDocument.update() ", e);
+                            });
+                } else {
+                    Toast.makeText(activity, "Rooms not present", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Log.e(TAG, "removeRooms: document doesn't exists ");
+            }
+        }).addOnFailureListener(e -> {
+            Log.e(TAG, "removeRooms: firestoreDocument.get() ", e);
         });
     }
 
