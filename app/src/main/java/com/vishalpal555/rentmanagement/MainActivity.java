d54d257a44b1951e1c6683c688017422aef3c159
Vishal Pal <vishalpal555@gmail.com>
2023-09-17 10:59:37 +0530

    re-added user email verification scheduler

--- app/src/main/java/com/vishalpal555/rentmanagement/MainActivity.java
+++ app/src/main/java/com/vishalpal555/rentmanagement/MainActivity.java
@@ -29,6 +29,9 @@ import com.vishalpal555.rentmanagement.service.RoomsFirestoreService;
 import com.vishalpal555.rentmanagement.service.Validator;
 
 import java.io.File;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -49,14 +52,6 @@ public class MainActivity extends AppCompatActivity {
     private Button connectDbBtn;
     private SharedPreferences sharedPreferences;
     @Override
-    public void onStart(){
-        super.onStart();
-        if(sharedPreferences.contains(Constants.SHARED_PREFERENCE_RENTMANAGEMENT_DOCUMENT_KEY)){
-            Intent rentManagementActivityIntent = new Intent(this, RentManagementActivity.class);
-            startActivity(rentManagementActivityIntent);
-        }
-    }
-    @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
@@ -76,7 +71,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
         firebaseUser = mAuth.getCurrentUser();
-        sharedPreferences = getApplicationContext().getSharedPreferences(Constants.SHARED_PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+
+        ScheduledExecutorService userReloadSchedule = Executors.newSingleThreadScheduledExecutor();
+        int delay = 0;
+        int period = 3;
+        if(!firebaseUser.isEmailVerified()){
+            userReloadSchedule.scheduleWithFixedDelay(() -> {
+                Log.d(TAG, "onCreate: checking if user verified.....");
+                firebaseUser.reload().addOnCompleteListener(firebaseUserReloadTask -> {
+                    if (firebaseUserReloadTask.isSuccessful()) {
+                        Log.i(TAG, "user reload successful: ");
+                        if (firebaseUser.isEmailVerified()) {
+                            Log.i(TAG, "user email verification done at: " +LocalDateTime.now());
+                            Log.i(TAG, "userReloadSchedule shutting down...... ");
+                            userReloadSchedule.shutdownNow();
+                            Log.i(TAG, "UI recreating..... ");
+                            runOnUiThread(this::recreate);
+                        }
+                    }
+                }).addOnFailureListener(e -> {
+                    Log.e(TAG, "user reload failed : ", e);
+                });
+            }, delay, period, TimeUnit.SECONDS);
+        }
+
+        createDbBtn = findViewById(R.id.createDatabaseBtn);
+        connectDatabaseInp = findViewById(R.id.connectDatabaseInp);
+        connectDbBtn = findViewById(R.id.connectDatabaseBtn);
 
         mainActivityTextTrial = findViewById(R.id.mainActivityTextTrial);
         mainActivityMenu = findViewById(R.id.mainActivityMenu);
@@ -90,19 +111,24 @@ public class MainActivity extends AppCompatActivity {
                 errorMsgMainActivity.setVisibility(View.GONE);
                 openGmailBtn.setVisibility(View.GONE);
 
+                sharedPreferences = getApplicationContext().getSharedPreferences(Constants.SHARED_PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+                if(sharedPreferences.contains(Constants.SHARED_PREFERENCE_RENTMANAGEMENT_DOCUMENT_KEY)){
+                    rentManagementFirestoreService = new RentManagementFirestoreService();
+                    rentManagementFirestoreService.connectRentManagementWithId(this, Objects.requireNonNull(sharedPreferences.getAll().get(Constants.SHARED_PREFERENCE_RENTMANAGEMENT_DOCUMENT_KEY)).toString());
+                }
 
                 rentManagementFirestoreService = new RentManagementFirestoreService();
-                createDbBtn = findViewById(R.id.createDatabaseBtn);
                 createDbBtn.setOnClickListener(view -> {
-                    rentManagementFirestoreService.createRentManagementWithId(this, firebaseUser.getEmail());
+                    rentManagementFirestoreService.createRentManagementWithId(this);
                 });
-                connectDatabaseInp = findViewById(R.id.connectDatabaseInp);
-                connectDbBtn = findViewById(R.id.connectDatabaseBtn);
                 connectDbBtn.setOnClickListener(view -> {
-                    rentManagementFirestoreService.connectRentManagementWithId(this, connectDatabaseInp.getText().toString(), firebaseUser.getEmail());
+                    rentManagementFirestoreService.connectRentManagementWithId(this, connectDatabaseInp.getText().toString());
                 });
 
             } else {
+                createDbBtn.setVisibility(View.GONE);
+                connectDatabaseInp.setVisibility(View.GONE);
+                connectDbBtn.setVisibility(View.GONE);
                 errorMsgMainActivity.setText(getString(R.string.verify_user_alert_msg));
                 errorMsgMainActivity.setVisibility(View.VISIBLE);
                 errorMsgMainActivity.bringToFront();
---
+++ app/src/main/java/com/vishalpal555/rentmanagement/service/TenantFirestoreService.java
@@ -0,0 +1,2 @@
+package com.vishalpal555.rentmanagement.service;public class TenantFirestoreService {
+}
